07b192c4c91ad88cf5b9a92c97df443f2febf1c1
Danny Zabaleta <99416451+Danny7w7@users.noreply.github.com>
2025-01-16 14:14:08 -0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -58,11 +58,11 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
